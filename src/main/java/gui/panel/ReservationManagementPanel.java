f267a2e9fc398d6bc9df24ac80b40c3fce90dfe4
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2025-09-06 10:09:04 +0700

    add dao entity service utill package

---
+++ src/main/java/gui/panel/ReservationManagementPanel.java
@@ -0,0 +1,75 @@
+package gui.panel;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.ui.FlatBorder;
+import gui.base.CustomUI;
+import gui.base.GridRoomPanel;
+import gui.base.RoomItem;
+
+import javax.sound.sampled.Line;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationManagementPanel extends JPanel {
+    private JLabel lblTop;
+    private GridRoomPanel gridRoomPanels;
+    private JPanel pnlTop;
+    private JScrollPane scrollPane;
+
+
+    public ReservationManagementPanel() {
+        setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+        init();
+    }
+    private void init(){
+        createTopPanel();
+        this.add(Box.createVerticalStrut(300));
+        createCenterPanel();
+    }
+    private void createTopPanel(){
+        pnlTop = new JPanel();
+        lblTop = new JLabel("Quản lý đặt phòng");
+        lblTop.setForeground(CustomUI.white);
+        pnlTop.setBackground(CustomUI.lightBlue);
+        pnlTop.add(lblTop);
+        pnlTop.setPreferredSize(new Dimension(0, 50));
+        add(pnlTop);
+    }
+
+    private void createCenterPanel(){
+        List<RoomItem> rooms = List.of(
+                new RoomItem("101", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("102", "Trống", "Phòng VIP Giường Đôi", null, null, new Color(100, 200, 100)),
+                new RoomItem("105", "Đang thuê", "Phòng Thường Giường Đơn", "Lê Thị B", "02-12-2024 12:18", new Color(80, 170, 250)),
+                new RoomItem("109", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("203", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("101", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("102", "Trống", "Phòng VIP Giường Đôi", null, null, new Color(100, 200, 100)),
+                new RoomItem("105", "Đang thuê", "Phòng Thường Giường Đơn", "Lê Thị B", "02-12-2024 12:18", new Color(80, 170, 250)),
+                new RoomItem("109", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("203", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("101", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("102", "Trống", "Phòng VIP Giường Đôi", null, null, new Color(100, 200, 100)),
+                new RoomItem("105", "Đang thuê", "Phòng Thường Giường Đơn", "Lê Thị B", "02-12-2024 12:18", new Color(80, 170, 250)),
+                new RoomItem("109", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("203", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("101", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("102", "Trống", "Phòng VIP Giường Đôi", null, null, new Color(100, 200, 100)),
+                new RoomItem("105", "Đang thuê", "Phòng Thường Giường Đơn", "Lê Thị B", "02-12-2024 12:18", new Color(80, 170, 250)),
+                new RoomItem("109", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100)),
+                new RoomItem("203", "Trống", "Phòng Thường Giường Đơn", null, null, new Color(100, 200, 100))
+        );
+        gridRoomPanels = new GridRoomPanel(rooms);
+        scrollPane = new JScrollPane(gridRoomPanels,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        setOpaque(true);
+        add(scrollPane);
+    }
+}
