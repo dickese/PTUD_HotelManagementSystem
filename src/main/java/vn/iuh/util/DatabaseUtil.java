7bf38e162510bc0d3d021e582d0c1f5548954569
hthinh24 <hungthinh240305@gmail.com>
2025-09-12 13:11:21 +0700

    update databaseUtil using env variable

--- src/main/java/vn/iuh/util/DatabaseUtil.java
+++ src/main/java/vn/iuh/util/DatabaseUtil.java
@@ -1,17 +1,23 @@
 package vn.iuh.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL =
-            "jdbc:sqlserver://localhost:1433;databaseName=hotel-management;encrypt=false;trustServerCertificate=true";
-    private static final String user = "sa";
-    private static final String pass = "Sapassword123";
+    private static final String url;
+    private static final String username;
+    private static final String password;
     private static Connection connection;
 
     static {
+        Dotenv dotenv = Dotenv.load();
+        url = dotenv.get("DB_URL");
+        username = dotenv.get("DB_USERNAME");
+        password = dotenv.get("DB_PASSWORD");
+
         connection = createConnection();
     }
 
@@ -39,7 +45,7 @@ public class DatabaseUtil {
     private static Connection createConnection() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(URL, user, pass);
+            connection = DriverManager.getConnection(url, username, password);
             if (connection != null) {
                 System.out.println("Kết nối thành công!");
             }
