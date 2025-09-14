3240dac9f1f9e49d8fe663420635008cfb6ef460
hthinh24 <hungthinh240305@gmail.com>
2025-09-14 15:07:15 +0700

    update increaseEntityID on EntityUtil

--- src/main/java/vn/iuh/util/EntityUtil.java
+++ src/main/java/vn/iuh/util/EntityUtil.java
@@ -2,11 +2,16 @@ package vn.iuh.util;
 
 public class EntityUtil {
     public static String increaseEntityID(String entityID, String prefix, int suffixLength) {
+        if (entityID == null) {
+            String format = "%0" + suffixLength + "d";
+            return prefix + String.format(format, 1);
+        }
+
         String[] strings = entityID.split(prefix);
         int id = Integer.parseInt(strings[1]);
         id++;
 
         String format = "%0" + suffixLength + "d";
-        return "RO" + String.format(format, id);
+        return prefix + String.format(format, id);
     }
 }
