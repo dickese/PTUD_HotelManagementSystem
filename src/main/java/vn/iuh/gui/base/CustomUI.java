61e9c30e6f5e25bf7f7fbe1c975cd4de485ad575
Duc Nguyen <nguyenvananhduc2005@gmail.com>
2025-09-15 09:36:05 +0700

    add dateChooser

--- src/main/java/vn/iuh/gui/base/CustomUI.java
+++ src/main/java/vn/iuh/gui/base/CustomUI.java
@@ -6,13 +6,14 @@ public class CustomUI {
     public static final Color darkBlue = new Color(50, 100, 155);
     public static final Color lightBlue = new Color(80, 170, 250);
     public static final Color white = Color.WHITE;
+    public static final Color black = Color.BLACK;
     public static final Color lightGreen = new Color(100, 220, 100);
     public static final Color darkGreen = new Color(80, 135, 80);
     public static final Color red = new Color(220, 35, 35);
     public static final Color purple = new Color(136, 40, 225);
     public static final Color gray = new Color(200, 200, 200);
-    public static final Font smallFont = new Font("Arial", Font.BOLD, 16);
-    public static final Font normalFont = new Font("Arial", Font.BOLD, 20);
-    public static final Font bigFont = new Font("Arial", Font.BOLD, 24);
+    public static final Font smallFont = new Font("Segoe UI", Font.BOLD, 16);
+    public static final Font normalFont = new Font("Segoe UI", Font.BOLD, 20);
+    public static final Font bigFont = new Font("Segoe UI", Font.BOLD, 24);
 
 }
